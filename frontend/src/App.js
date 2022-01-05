1b4edb31bf5d6fb6237fab4c85acdac890efc31b
georgeclifford <grgclffrd@gmail.com>
2022-01-05 10:53:14 +0530

    changed h1 text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World!
+         My First React Change
         </p>
         <a
           className="App-link"
